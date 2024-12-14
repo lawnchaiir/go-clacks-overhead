6fba8e3f4bc8c230f4445e8ed4baa679afd4d867
lawnchaiir <lawnchaiir@users.noreply.github.com>
2024-12-14 11:37:35 -0500

    resaving to pick up change

--- http/main.go
+++ http/main.go
@@ -7,11 +7,6 @@ import (
 
 func main() {
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		/*if r.Method != http.MethodGet {
-			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-			return
-		}*/
-
 		w.Header().Set("Content-Type", "application/json")
 
 		json.NewEncoder(w).Encode(struct {
